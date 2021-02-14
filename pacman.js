20b62a8a5bc39e272f34adb36f41e87233e4bdce
Shamil <badalloffsh@gmail.com>
2021-02-14 10:50:36 +0100

    Add: constraints to moce through gost lair

--- pacman.js
+++ pacman.js
@@ -55,6 +55,8 @@ function createBoard() {
             squares[i].classList.add('pac-dot')
         } else if (layout[i] === 1) {
             squares[i].classList.add('wall')
+        } else if (layout[i] === 2) {
+            squares[i].classList.add('ghost-lair')
         } else if (layout[i] === 3) {
             squares[i].classList.add('power-pellet')
         }
@@ -77,25 +79,39 @@ function control(e) {
     switch(e.keyCode) {
         case 40:
         console.log('pressed down')
-        if (!squares[pacmanCurrentIndex + width].classList.contains('wall') &&
+        if (
+            !squares[pacmanCurrentIndex + width].classList.contains('ghost-lair') &&
+            !squares[pacmanCurrentIndex + width].classList.contains('wall') &&
             pacmanCurrentIndex + width < width * width
             ) 
             pacmanCurrentIndex += width
         break
         case 38:
         console.log('pressed up')
-        if (!squares[pacmanCurrentIndex - width].classList.contains('wall') &&
-            pacmanCurrentIndex - width >=0) pacmanCurrentIndex -= width
+        if (
+            !squares[pacmanCurrentIndex -width].classList.contains('ghost-lair') &&
+            !squares[pacmanCurrentIndex - width].classList.contains('wall') &&
+            pacmanCurrentIndex - width >=0
+            ) 
+            pacmanCurrentIndex -= width
         break
         case 37: 
         console.log('pressed left')
-        if(!squares[pacmanCurrentIndex - 1].classList.contains('wall') &&
-            pacmanCurrentIndex % width !==0) pacmanCurrentIndex -=1
+        if( 
+            !squares[pacmanCurrentIndex -1].classList.contains('ghost-lair') &&
+            !squares[pacmanCurrentIndex -1].classList.contains('wall') &&
+            pacmanCurrentIndex % width !==0
+            ) 
+            pacmanCurrentIndex -=1
         break
         case 39:
         console.log('pressed right')
-        if(!squares[pacmanCurrentIndex + 1].classList.contains('wall') &&
-            pacmanCurrentIndex % width < width -1) pacmanCurrentIndex +=1
+        if(
+            !squares[pacmanCurrentIndex +1].classList.contains('ghost-lair') &&
+            !squares[pacmanCurrentIndex +1].classList.contains('wall') &&
+            pacmanCurrentIndex % width < width -1
+            ) 
+            pacmanCurrentIndex +=1
         break
     }
     squares[pacmanCurrentIndex].classList.add('pacman')
